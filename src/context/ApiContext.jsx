f5e0756e3a6fa049bf11069fbf5d356a9fc3322d
HAMZA MHALLI <hamzamhalli95@gmail.com>
2023-05-17 16:16:53 +0100

    change API

--- src/context/ApiContext.jsx
+++ src/context/ApiContext.jsx
@@ -4,7 +4,7 @@ import { createContext} from "react";
 export const ApiContext = createContext();
 
 export  function ApiContextProvider(Props) {
-    const Api_url = "http://localhost:3000";
+    const Api_url = "https://lmsapi-mhallihamza.onrender.com";
   return (
     <ApiContext.Provider value={{Api_url}}>
         {Props.children}
